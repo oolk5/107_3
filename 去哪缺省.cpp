0ac5f54421a00cff4e3eaeb257cd509ff04f1c2f
oolk5 <143987221+oolk5@users.noreply.github.com>
2024-05-03 00:18:10 +0800

    Add files via upload

---
+++ 去哪缺省.cpp
@@ -0,0 +1,38 @@
+// Created on iPad.
+
+#include <iostream>
+using namespace std;
+class Name
+{
+    public:
+    Name(int =10,int =11);
+    Name operator+(Name&);
+    void display();
+    private:
+    int phone;
+    int number;
+};
+Name Name::operator+(Name&npp)
+{
+    return Name(phone+npp.phone,number+npp.number);
+}
+Name::Name(int a,int b)
+{
+phone=a;
+number=b;
+}
+void Name::display()
+{
+    cout<<this->phone<<"  "<<this->number<<endl;
+}
+int main() 
+{
+    
+    Name name1(1,3),name2(2,3),name3;
+    name3=name1+name2;
+    name1.display();
+    name1.display();
+    name3.display();
+
+    return 0;
+}
